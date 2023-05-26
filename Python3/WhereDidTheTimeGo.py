a3b350d354595c3647b08cddcaa4f642b1878418
kitty-panics <e222d29f@outlook.com>
2023-05-26 12:41:38 -0400

    feat: 上传 Python3 版本。

---
+++ Python3/WhereDidTheTimeGo.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+#
+# 统计最近 10 天呼出、呼入时长
+#
+
+from androidhelper import sl4a
+from datetime import datetime, timedelta
+import time
+
+# 获取所有通话记录
+pydroid = sl4a.Android()
+uricontent = pydroid.getConstants("android.provider.CallLog$Calls").result
+calllog = pydroid.queryContent(uricontent['CONTENT_URI'], ["number", "duration", "date", "type"]).result
+
+# 定义存放最近 10 天呼出、呼入时长的字典
+# 呼出类型值: 2、呼入类型值: 1
+ten_days = {}
+for i in range(0, 10):
+    reduce_day = datetime.now() - timedelta(days = i)
+    format_day = reduce_day.strftime('%Y-%m-%d')
+    ten_days[format_day] = [[], []]
+
+# 遍历通话记录，并将呼出、呼入时长存入字典
+# 呼出类型值: 2、呼入类型值: 1
+calllog_len = len(calllog)
+for i in range(0, calllog_len):
+    calllog_info = calllog[i]
+    call_time = calllog_info['duration']
+    call_date = time.strftime("%Y-%m-%d", time.localtime(int(calllog_info['date']) / 1000))
+    call_type = calllog_info['type']
+    if call_date in ten_days:
+        if call_type == '2':
+            ten_days[call_date][0].append(int(call_time))
+        elif call_type == '1':
+            ten_days[call_date][1].append(int(call_time))
+
+# 计算出最近 10 天的呼出、呼入时长
+for i in ten_days:
+    # 呼出
+    out_daily_data = ten_days[i][0]
+    out_sum_sec = sum(out_daily_data)
+    out_sum_min = round(out_sum_sec / 60, 2)
+    out_sum_count = len(out_daily_data)
+    out_tmp_list = [j for j in out_daily_data if j != 0]
+    out_sum_count_c = len(out_tmp_list)
+    # 呼入
+    in_daily_data = ten_days[i][1]
+    in_sum_sec = sum(in_daily_data)
+    in_sum_min = round(in_sum_sec / 60, 2)
+    in_sum_count = len(in_daily_data)
+    in_tmp_list = [j for j in in_daily_data if j != 0]
+    in_sum_count_c = len(in_tmp_list)
+    print(i + ': 出: ' + str(out_sum_count_c) + '/' + str(out_sum_count) + '次、' + str(out_sum_sec) + '秒(' + str(out_sum_min) + '分)')
+    print('            入: ' + str(in_sum_count_c) + '/' + str(in_sum_count) + '次、' + str(in_sum_sec) + '秒(' + str(in_sum_min) + '分)')
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # WhereDidTheTimeGo
-[Py3/Java] 统计最近 10 天呼出、呼入时长 (Count the length of outgoing and incoming calls in the last 10 days.)
+
+统计最近 10 天呼出、呼入时长。
+
+## 使用
+
+一共有两个版本，Python3 版和 Java 版，相对来说 Java 版比 Python3
+版功能更多，速度更快。
+
+### Python 版
+
+需通过 [QPython3]、[QPython3C] 运行，并且 [QPython3]、[QPython3C]
+需要启用 "读取通话记录" 权限。
+
+[QPython3]: https://github.com/qpython-android/qpython3
+[QPython3C]: https://gitee.com/buddygr/qpython-3c
+
+### Java 版
+
+直接 [下载] 安装即可，使用前同样需要启用 "读取通话记录" 权限。
+
+[下载]: https://github.com/kitty-panics/WhereDidTheTimeGo/releases
